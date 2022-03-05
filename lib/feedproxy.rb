04fe714b448b83d4c2fb9b1bfe0797a82ad02688
Dennis Heckman <denheck@gmail.com>
2022-03-05 10:44:48 -0600

    initial create from command: bundle gem --exe --mit --test=rspec --ci=github --linter=rubocop feedproxy

---
+++ lib/feedproxy.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "feedproxy/version"
+
+module Feedproxy
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/feedproxy/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Feedproxy
+  VERSION = "0.1.0"
+end
